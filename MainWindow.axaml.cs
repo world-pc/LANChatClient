c8532c731939ba25b2d205678b846e4d628fda0a
chris <287334486+world-pc@users.noreply.github.com>
2026-06-26 10:23:39 -0400

    Have UdpChatService listening on a UDP port on startup.

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -2,10 +2,16 @@ using Avalonia.Controls;
 
 namespace LANChatClient;
 
-public partial class MainWindow : Window
-{
-    public MainWindow()
-    {
+public partial class MainWindow : Window {
+    
+    private UdpChatService _chatService;
+
+    public MainWindow() {
         InitializeComponent();
+
+        _chatService = new UdpChatService(9000);
+        _ = _chatService.StartListeningAsync(message => {
+            chatlog.Items.Add(message);           
+        });
     }
 }
--- UdpChatService.cs
+++ UdpChatService.cs
@@ -1,19 +1,28 @@
+using System;
+using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+namespace LANChatClient;
+
 public class UdpChatService {
 
-    private UdpClient _listener;
+    private UdpClient _listener, _sender;
 
     public UdpChatService(int listeningPort) {
-        _listener = UdpClient(listeningPort);
+        _listener = new UdpClient(listeningPort);
+
+        _sender = new UdpClient();
     }
 
     public async Task StartListeningAsync(Action<string> onMessageReceived) {
         while(true) {
-            UdpReceiveResult result = await udpClient.ReceiveAsync();
+            UdpReceiveResult result = await _listener.ReceiveAsync();
             onMessageReceived(Encoding.UTF8.GetString(result.Buffer));
         }
+    }
+
+    public void SendMessage(string given_msg) {
     }
 }
